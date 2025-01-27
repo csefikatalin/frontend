a452278d225097390b7f8ff2ae3e4ad4f4e50354
csefikatalin <csefi.katalin@gmail.com>
2025-01-27 17:07:12 +0100

    UserLayout és Admin Layout jogosultság szerint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Kezdolap from "./pages/Kezdolap";
 import Bejelentkezes from "./pages/Bejelentkezes";
 import Regisztracio from "./pages/Regisztracio";
@@ -6,24 +6,39 @@ import VendegLayout from "./layouts/VendegLayout";
 import AdminLayout from "./layouts/AdminLayout";
 import KezdolapUser from "./pages/KezdolapUser";
 import UserLayout from "./layouts/UserLayout";
+import useAuthContext from "./contexts/AuthContext";
 
 
 function App() {
+    const { user } = useAuthContext();
     return (
         <>
 
             <Routes>
-                <Route  element={<AdminLayout />}>
-                    <Route path="/" element={<Kezdolap />} />
-                </Route>
-                <Route  element={<UserLayout />}>
-                    <Route path="/" element={<KezdolapUser />} />
-                </Route>
-                <Route  element={<VendegLayout />}>
-                    <Route path="bejelentkezes" element={<Bejelentkezes />} />
-                    <Route path="regisztracio" element={<Regisztracio />} />
+                {/* Vendég layout */}
+                {!user && (
+                    <Route element={<VendegLayout />}>
+                        <Route path="/" element={<Kezdolap />} />
+                        <Route path="bejelentkezes" element={<Bejelentkezes />} />
+                        <Route path="regisztracio" element={<Regisztracio />} />
+                    </Route>
+                )}
 
-                </Route>
+                {/* Admin és User ugyanazon útvonalon */}
+                {user && (
+                    <Route
+                        path="/"
+                        element={
+                            user.role === 1 ? (
+                                <AdminLayout />
+                            ) : (
+                                <UserLayout />
+                            )
+                        }
+                    >
+                        <Route index element={<Kezdolap />} />
+                    </Route>
+                )}
             </Routes>
         </>
     );
